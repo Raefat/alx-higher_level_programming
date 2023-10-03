16e2b7187324451601a3cc226aa409409b86611a
Raefat <raefetex@gmail.com>
2023-10-03 03:38:34 -0700

    fixed task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     upper = ''
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
-            letter = char(ord(letter) - 32)
+            letter = chr(ord(letter) - 32)
         print("{:s}".format(letter), end='')
     print()
