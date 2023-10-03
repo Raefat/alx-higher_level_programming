27cc548b00577dfad81f7780eb243700ec385bb8
Raefat <raefetex@gmail.com>
2023-10-03 03:29:30 -0700

    fixed task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for num in range(0, 99):
-    if num < 10:
-        print("{:02d}".format(num), end=', ')
+    print("{:02d}".format(num), end=', ')
 print("{:02d}".format(num + 1))
