a71aa78702c188d2c8d2a206d4efd5eb0e3ff6cc
Raefat <raefetex@gmail.com>
2023-10-03 04:00:32 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last = (number % 10) - 10 if number < 0 else number % 10
+    last = 10 - (number % 10) if number < 0 else number % 10
     print("{}".format(last), end='')
     return last
