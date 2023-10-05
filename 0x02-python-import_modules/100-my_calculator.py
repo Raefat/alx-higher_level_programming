013e893680b1ac42433aabbbd8c48f2e8129fdc2
Raefat <raefetex@gmail.com>
2023-10-05 09:22:01 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[0])
-    b = int(sys.argv[2])
-    op = sys.argv[1]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
     if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
         sys.exit(0)
