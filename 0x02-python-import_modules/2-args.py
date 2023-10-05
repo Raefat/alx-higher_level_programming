13356d61ebf99cf4fcc56ed036d0e449e6a74940
Raefat <raefetex@gmail.com>
2023-10-05 04:38:38 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif arg_len == 1:
         print("1 argument:")
-        print("{}: {}".format(arg_len, sys.argv[arg_len])
+        print("{}: {}".format(arg_len, sys.argv[arg_len]))
     else:
         print("{}: arguments:".format(arg_len))
         for arg in range(1, arg_len + 1):
-            print("{}: {}".format(arg, sys.argv[arg_len])
+            print("{}: {}".format(arg, sys.argv[arg_len]))
