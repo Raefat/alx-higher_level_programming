c01fba577421d54c26e48cef0460c70e7b3be2f9
Raefat <raefetex@gmail.com>
2023-10-07 11:16:13 -0700

    task 8

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    is_divis = []
+    for el in my_list:
+        if el % 2 == 0:
+            is_divis.append(True)
+        else:
+            is_divis.append(False)
+     return is_divis
