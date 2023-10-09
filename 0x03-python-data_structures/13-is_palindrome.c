6d46dcf36dccd76b76597c7d0f3358a46c21fe40
Raefat <raefetex@gmail.com>
2023-10-09 16:48:48 -0700

    Interview Question

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,71 @@
-#include "lists.h"
 
 /**
- * reverse - reverse a linked list
- * @head: pointer to the LL
- * Return: pointer to reversed LL
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
  */
-
-listint_t *reverse(listint_t *head)
+void reverse_listint(listint_t **head)
 {
-	listint_t prev = NULL;
-	listint_t current = head;
-	listint_t next = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-	while (current != NULL)
+	while (current)
 	{
 		next = current->next;
 		current->next = prev;
 		prev = current;
 		current = next;
 	}
-	return (prev);
+
+	*head = prev;
 }
 
 /**
- * is_palindrome - function that checks if a LL is palindrome
- * @head: pointer to linked list
- * Return: 1 if true 0 if false
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t secondHalf;
-	listint_t *fast = head;
-	listint_t *slow = head;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (head == NULL || head->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (fast != NULL && fast->next != NULL)
+	while (1)
 	{
-		slow = slow->next;
 		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	secondHalf = reverse(slow);
-	while (secondHalf != NULL)
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
 	{
-		if (head->data != secondHalf->data)
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
 			return (0);
-		head = head->next;
-		secondHalf = secondHalf->next;
 	}
-	return (1);
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
