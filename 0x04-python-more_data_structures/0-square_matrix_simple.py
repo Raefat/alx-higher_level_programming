aebdd6b4e1249f1f59e7ad1c311213eb711c1373
Raefat <raefetex@gmail.com>
2023-10-11 11:02:54 -0700

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    sqrt_matrix = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            sqrt_matrix[i][j] = matrix[i][j] ** 2
+    return sqrt_matrix
