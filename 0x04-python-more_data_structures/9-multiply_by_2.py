e88aeec56deda03067bdf24cbacf288eba44151a
Raefat <raefetex@gmail.com>
2023-10-11 12:41:26 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for k in a_dictionary:
-        a_dictionary[k] = a_dictionary[k] * 2
-    return a_dictionary
+    a_dict = a_dictionary.copy()
+    for k in a_dict:
+        a_dict[k] = a_dictionary[k] * 2
+    return a_dict
