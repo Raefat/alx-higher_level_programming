82c8a3e45bea48baafbf0418f9b0c8deb1b3ddd0
Raefat <raefetex@gmail.com>
2023-10-13 06:55:17 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqrt_matrix = []
+    sqrt_matrix = matrix.copy()
     for i in range(len(matrix)):
-        sqrt_matrix.append([])
         for j in range(len(matrix[i])):
             sqrt_matrix[i][j] = matrix[i][j] ** 2
     return sqrt_matrix
