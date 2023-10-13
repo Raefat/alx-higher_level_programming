e4bd43d7af33427b02e19eb8a01c74acb66ff830
Raefat <raefetex@gmail.com>
2023-10-13 07:57:48 -0700

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,4 +3,9 @@ def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
     else:
-       return  sum(my_list) / len(my_list)
+        avg = 0
+        weights = 0
+        for i in range(len(my_list)):
+            weights += my_list[i][1]
+            avg += my_list[i][0] * my_list[i][1] 
+        return  avg / weights
