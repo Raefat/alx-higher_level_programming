42b4ca83e75e050601b5bb15fab0007d5e029a95
Raefat <raefetex@gmail.com>
2023-10-24 08:30:08 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Sqaure Module """
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size):
+        """ Initialize new Square
+            Args:
+                size (int): the size of the new square.
+        """
+        self.__size = size
