c0d1734b6918f91aa14167ccbca505714ecd8fcb
Raefat <raefetex@gmail.com>
2023-10-24 08:56:36 -0700

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""" Sqaure Module """
+
+
+class Square:
+    """ Square Class """
+    def __init__(self, size=0):
+        """ Initialize new Square
+            Args:
+                size (int): the size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculates the area of a square """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Prints the square """
+        for i in range(self.__size ** 2):
+            if i % self.__size == 0:
+                print("\n")
+            else:
+                print("#")
