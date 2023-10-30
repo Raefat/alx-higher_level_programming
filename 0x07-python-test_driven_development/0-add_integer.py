df11634cca70e997768b5eea68f945e2938cffdf
Raefat <raefetex@gmail.com>
2023-10-30 10:03:47 -0700

    task 1 to 8

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ function that adds two numbers
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if not isinstance(a, float) and not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, float) and not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
